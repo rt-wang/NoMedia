e21c556f8152adbd796b713a2df05aec40def02c
Fodenthal <155864553+Fodenthal@users.noreply.github.com>
2024-09-18 13:23:35 -0400

    Create Page header adjustment

--- CreatePage.js
+++ CreatePage.js
@@ -204,6 +204,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginTop: -10,
     marginBottom: 16,
   },
   savedIndicator: {
