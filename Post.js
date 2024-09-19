d88800d217a2fc8f4b332dd2c5028765e48037ee
Fodenthal <155864553+Fodenthal@users.noreply.github.com>
2024-09-19 11:27:50 -0400

    heart color

--- Post.js
+++ Post.js
@@ -117,7 +117,7 @@ const Post = ({ item, onCommentPress, isQuoteRepost = false }) => {
             <Ionicons 
               name={isLiked ? "heart" : "heart-outline"} 
               size={18} 
-              color={isLiked ? "red" : "gray"} 
+              color={isLiked ? "white" : "white"} 
             />
             <Text style={styles.toolCount}>{item.likes}</Text>
           </TouchableOpacity>
