350ab5913c294539b32dc2d9e27ec81cbdc0ff27
rt-wang <rtwang18@yahoo.com>
2024-09-24 17:41:19 +0800

    FIXED BUGS

--- Backend/common/src/main/java/com/example/common/CommonApplication.java
+++ Backend/common/src/main/java/com/example/common/CommonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.example.common"})
+@ComponentScan(basePackages = {"com.example.common"})
 public class CommonApplication {
 
 	public static void main(String[] args) {
