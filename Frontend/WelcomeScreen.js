229e5ed491f8e918e02e409e2c864e424cc5adc0
Fodenthal <155864553+Fodenthal@users.noreply.github.com>
2024-10-11 23:09:52 -0500

    All marketing stuff and merged but posts do not show up on for you page (the created ones) bc of marketing override need to fix

--- Frontend/WelcomeScreen.js
+++ Frontend/WelcomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const WelcomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}><Text style={styles.welcomeTextBold}></Text>Waitlist in Bio</Text>
+      <Text style={styles.welcomeText}>We all need mental exercise</Text>
     </View>
   );
 };
@@ -17,15 +17,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   welcomeText: {
-    fontSize: 32,
+    fontSize: 30,
     color: '#FFFFFF',
     fontFamily: 'Athelas-Bold', // Note: You may need to ensure this font is available in your project
     textAlign: 'center',
-    left: 1,
-  },
-  welcomeTextBold: {
-    fontFamily: 'Athelas-Regular',
-    fontSize: 47,
+    left: 0,
   },
 });
 
